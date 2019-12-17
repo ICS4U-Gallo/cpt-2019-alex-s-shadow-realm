c4c58c201db5ae91e224045907045b35042ec199
raydatoo <rayhaandatoo@hotmail.com>
2019-12-17 16:55:17 -0500

    created my view

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import settings
 
 from menu import MenuView
 from chapter_1 import Chapter1View
+from rays_level import RayView
 
 
 class Director(arcade.Window):
@@ -12,7 +13,8 @@ class Director(arcade.Window):
         self.view_index = 0
         self.views = [
             MenuView,
-            Chapter1View
+            Chapter1View,
+            RayView
         ]
         self.next_view()
 
