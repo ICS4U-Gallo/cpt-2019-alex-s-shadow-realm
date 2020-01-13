af020589b791a4aec6067ba7ecadc37d24d63fa3
Alex Passarelli <alexpassarelli@Alexs-MacBook.local>
2020-01-13 11:10:48 -0500

    idk if im doing this right

--- alex_level.py
+++ alex_level.py
@@ -1,20 +1,40 @@
 import arcade
 
 import settings
-
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+COIN_SCALING = 0.5
 
 class Alexlevel(arcade.View):
-    def on_show(self):
+    def __init__(self,):
+        self.wall_list = None
+        self.player_list = None
+        self.coin_list = None
+
+        self.player_sprite = None
+        
         arcade.set_background_color(arcade.color.WHITE)
 
     def on_draw(self):
         arcade.start_render()
         arcade.draw_text("Alex Level", settings.WIDTH/2, settings.HEIGHT/2,
                          arcade.color.BLACK, font_size=30, anchor_x="center")
+                         
+        self.player_list.draw()
 
     def on_key_press(self, key, modifiers):
         self.director.next_view()
 
+    def setup():
+
+        self.player_list = arcade.SpriteList()
+
+        # Set up the player, specifically placing it at these coordinates.
+        image_source = "/Users/alexpassarelli/Downloads/cpt/cloud.jpg"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
 
 if __name__ == "__main__":
     """This section of code will allow you to run your View
