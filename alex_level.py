043a5ad7ac9420166dc025113ae074a2d2811fb2
stickilas <43246545+stickilas@users.noreply.github.com>
2020-01-15 14:28:27 -0500

    djandjahdbajhdbaskdnaskdjanslkdjsan PHYSICS ENGINE

--- alex_level.py
+++ alex_level.py
@@ -1,97 +1,127 @@
 import arcade
 
 import settings
-CHARACTER_SCALING = 0.2
+CHARACTER_SCALING = 2
 TILE_SCALING = 0.5
 # COIN_SCALING = 0.5
 
+# Constants used to track if the player is facing left or right
+RIGHT_FACING = 0
+LEFT_FACING = 1
+
 MOVEMENT_SPEED = 5
 UPDATES_PER_FRAME = 7
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
 
-class PlayerCharacter(arcade.Sprite):
-    def __init__(self):
 
-        # Set up parent class
-        super().__init__()
+# def load_texture_pair(filename):
+#     """
+#     Load a texture pair, with the second being a mirror image.
+#     """
+#     return [
+#         arcade.load_texture(filename, scale=CHARACTER_SCALING),
+#         arcade.load_texture(filename, scale=CHARACTER_SCALING, mirrored=True)
+#     ]
 
-        # Default to face-right
-        self.character_face_direction = RIGHT_FACING
+# class PlayerCharacter(arcade.Sprite):
+#     def __init__(self):
 
-        # Used for flipping between image sequences
-        self.cur_texture = 0
+#         # Set up parent class
+#         super().__init__()
 
-        # Track out state
-        self.jumping = False
-        self.climbing = False
-        self.is_on_ladder = False
+#         # Default to face-right
+#         self.character_face_direction = RIGHT_FACING
 
-        # Adjust the collision box. Default includes too much empty space
-        # side-to-side. Box is centered at sprite center, (0, 0)
-        self.points = [[-22, -64], [22, -64], [22, 28], [-22, 28]]
+#         # Used for flipping between image sequences
+#         self.cur_texture = 0
 
-        # --- Load Textures ---
+#         # Track out state
+#         self.jumping = False
+#         self.climbing = False
+#         self.is_on_ladder = False
 
-        # Images from Kenney.nl's Asset Pack 3
-        main_path = ":resources:images/animated_characters/female_adventurer/femaleAdventurer"
-        # main_path = ":resources:images/animated_characters/female_person/femalePerson"
-        # main_path = ":resources:images/animated_characters/male_person/malePerson"
-        # main_path = ":resources:images/animated_characters/male_adventurer/maleAdventurer"
-        # main_path = ":resources:images/animated_characters/zombie/zombie"
-        # main_path = ":resources:images/animated_characters/robot/robot"
+#         # Adjust the collision box. Default includes too much empty space
+#         # side-to-side. Box is centered at sprite center, (0, 0)
+#         self.points = [[-22, -64], [22, -64], [22, 28], [-22, 28]]
 
-        # Load textures for idle standing
-        self.idle_texture_pair = load_texture_pair("2 Owlet_Monster/Owlet_Monster_Idle_4.png")
+#         # --- Load Textures ---
 
-        # Load textures for walking
-        self.walk_textures = []
-        for i in range(8):
-            texture = load_texture_pair(f"{main_path}_walk{i}.png")
-            self.walk_textures.append(texture)
+#         # Images from 
+#         main_path = "cpt-2019-alex-s-shadow-realm/images/Alex Images/Owlet_Monster.png"
+#         # main_path = ":resources:images/animated_characters/female_person/femalePerson"
 
-    def update_animation(self, delta_time: float = 1/60):
 
-        # Figure out if we need to flip face left or right
-        if self.change_x < 0 and self.character_face_direction == RIGHT_FACING:
-            self.character_face_direction = LEFT_FACING
-        elif self.change_x > 0 and self.character_face_direction == LEFT_FACING:
-            self.character_face_direction = RIGHT_FACING
+#         # Load textures for idle standing
+#         self.idle_texture_pair = load_texture_pair("cpt-2019-alex-s-shadow-realm/images/Alex Images/Owlet_Monster_Idle_4.png")
 
-        # Idle animation
-        if self.change_x == 0 and self.change_y == 0:
-            self.texture = self.idle_texture_pair[self.character_face_direction]
-            return
+#         # Load textures for walking
+#         self.walk_textures = []
+#         for i in range(6):
+#             texture = load_texture_pair("cpt-2019-alex-s-shadow-realm/images/Alex Images/Owlet_Monster_Walk_6.png")
+#             self.walk_textures.append(texture)
 
-        # Walking animation
-        self.cur_texture += 1
-        if self.cur_texture > 7 * UPDATES_PER_FRAME:
-            self.cur_texture = 0
-        self.texture = self.walk_textures[self.cur_texture // UPDATES_PER_FRAME][self.character_face_direction]
+    # def update_animation(self, delta_time: float = 1/60):
+
+    #     # Figure out if we need to flip face left or right
+    #     if self.change_x < 0 and self.character_face_direction == RIGHT_FACING:
+    #         self.character_face_direction = LEFT_FACING
+    #     elif self.change_x > 0 and self.character_face_direction == LEFT_FACING:
+    #         self.character_face_direction = RIGHT_FACING
+
+    #     # Idle animation
+    #     if self.change_x == 0 and self.change_y == 0:
+    #         self.texture = self.idle_texture_pair[self.character_face_direction]
+    #         return
+
+    #     # Walking animation
+    #     self.cur_texture += 1
+    #     if self.cur_texture > 7 * UPDATES_PER_FRAME:
+    #         self.cur_texture = 0
+    #     self.texture = self.walk_textures[self.cur_texture // UPDATES_PER_FRAME][self.character_face_direction]
+
+
+
 
 class Alexlevel(arcade.View):
+
+
     def __init__(self):
+
+
         super().__init__()
+
+
         arcade.set_background_color(arcade.color.WHITE)
 
         self.wall_list = arcade.SpriteList()
         self.coin_list = arcade.SpriteList()
         self.player_list = arcade.SpriteList()
 
-# cloud image
-        # image_source = "/Users/alexpassarelli/Downloads/cpt/cloud.jpg"
-        # self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
-        # self.player_sprite.center_x = 64
-        # self.player_sprite.center_y = 128
-        # self.player_list.append(self.player_sprite)
+        # Physics Engine
+        self.physics_engine = None
+
+        # Set up the player
+        image_source = "cpt-2019-alex-s-shadow-realm/images/Alex Images/Owlet_Monster.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
        
-        self.background = arcade.load_texture("/Users/alexpassarelli/Downloads/cpt/glacial_mountains_preview_lightened.png")
+        self.background = arcade.load_texture("cpt-2019-alex-s-shadow-realm/images/Alex Images/glacial_mountains_preview_lightened.png")
         
         # Create the ground
 
         for x in range(0, settings.WIDTH, 64):
-            wall = arcade.Sprite("/Users/alexpassarelli/Downloads/Free Platform Game Assets/Update 1.9/New Tiles (2D view)/Winter/128x128/Grass.png", TILE_SCALING)
+            wall = arcade.Sprite("cpt-2019-alex-s-shadow-realm/images/Alex Images/Free Platform Game Assets/Update 1.9/New Tiles (2D view)/Winter/128x128/GrassJoinHillLeft2 DownShadow.png", TILE_SCALING)
             wall.center_x = x
             wall.center_y = 32
             self.wall_list.append(wall)
+
+
+        # Adding Coins
+
 
     def on_draw(self):
         arcade.start_render()
@@ -104,21 +134,38 @@ class Alexlevel(arcade.View):
 
         self.player_list.draw()
 
-        self.player = PlayerCharacter
+        # Physics Engine
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
 
     def on_key_press(self, key, modifiers):
         """
         Called whenever a key is pressed.
         """
-        if key == arcade.key.UP:
-            self.player.change_y = MOVEMENT_SPEED
-        elif key == arcade.key.DOWN:
-            self.player.change_y = -MOVEMENT_SPEED
-        elif key == arcade.key.LEFT:
-            self.player.change_x = -MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT:
-            self.player.change_x = MOVEMENT_SPEED
+        if key == arcade.key.UP or key == arcade.key.W:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+    
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """ 
 
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+
+        # Move the player with the physics engine
+
+
+        self.coin_list.update()
+        self.coin_list.update_animation()
+        self.player_list.update()
+        self.player_list.update_animation()
 
 if __name__ == "__main__":
     """This section of code will allow you to run your View
