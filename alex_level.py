d3afe1065d7c6548b4360236fea36ad3f4633339
stickilas <stickilas@gmail.com>
2020-01-19 14:48:38 -0500

    adding map shenanigans now

--- alex_level.py
+++ alex_level.py
@@ -9,15 +9,17 @@ TILE_SCALING = 0.5
 RIGHT_FACING = 0
 LEFT_FACING = 1
 
-MOVEMENT_SPEED = 3
+MOVEMENT_SPEED = 4
 UPDATES_PER_FRAME = 7
 GRAVITY = 0.65
 PLAYER_JUMP_SPEED = 13
 
-LEFT_VIEWPORT_MARGIN = 250
+IMAGE_WIDTH = 1350
+SCROLL_SPEED = 5
+
+LEFT_VIEWPORT_MARGIN = 100
 RIGHT_VIEWPORT_MARGIN = 250
-BOTTOM_VIEWPORT_MARGIN = 50
-TOP_VIEWPORT_MARGIN = 100
+
 
 def load_texture_pair(filename):
     """
@@ -27,68 +29,6 @@ def load_texture_pair(filename):
         arcade.load_texture(filename, scale=CHARACTER_SCALING),
         arcade.load_texture(filename, scale=CHARACTER_SCALING, mirrored=True)
     ]
-
-# class PlayerCharacter(arcade.Sprite):
-#     def __init__(self):
-
-#         # Set up parent class
-#         super().__init__()
-
-#         # Default to face-right
-#         self.character_face_direction = RIGHT_FACING
-
-#         # Used for flipping between image sequences
-#         self.cur_texture = 0
-
-#         # Track out state
-#         self.jumping = False
-#         self.climbing = False
-#         self.is_on_ladder = False
-
-#         # Adjust the collision box. Default includes too much empty space
-#         # side-to-side. Box is centered at sprite center, (0, 0)
-#         self.points = [[-22, -64], [22, -64], [22, 28], [-22, 28]]
-
-#         # --- Load Textures ---
-
-#         # Images from 
-#         main_path = "cpt-2019-alex-s-shadow-realm/images/Alex Images/animations/"
-#         # main_path = ":resources:images/animated_characters/female_person/femalePerson"
-
-
-#         # Load textures for idle standing
-#         self.idle_texture_pair = load_texture_pair("cpt-2019-alex-s-shadow-realm/images/Alex Images/Owlet_Monster.png")
-#         self.idle_textures = []
-#         for i in range(4):
-#             texture = load_texture_pair(f"{main_path}idle00{i}.png")
-#             self.idle_textures.append(texture)
-
-#         # Load textures for walking
-#         self.walk_textures = []
-#         for i in range(6):
-#             texture = load_texture_pair(f"{main_path}tile00{i}.png")
-#             self.walk_textures.append(texture)
-
-#     def update_animation(self, delta_time: float = 1/60):
-
-#         # Figure out if we need to flip face left or right
-#         if self.change_x < 0 and self.character_face_direction == RIGHT_FACING:
-#             self.character_face_direction = LEFT_FACING
-#         elif self.change_x > 0 and self.character_face_direction == LEFT_FACING:
-#             self.character_face_direction = RIGHT_FACING
-
-#         # Idle animation
-#         if self.change_x == 0 and self.change_y == 0:
-#             self.texture = self.idle_texture_pair[self.character_face_direction]
-#             return
-
-#         # Walking animation
-#         self.cur_texture += 1
-#         if self.cur_texture < 7 * UPDATES_PER_FRAME:
-#             self.cur_texture = 0
-#         self.texture = self.walk_textures[self.cur_texture // UPDATES_PER_FRAME][self.character_face_direction]
-
-
 
 
 class Alexlevel(arcade.View):
@@ -113,62 +53,79 @@ class Alexlevel(arcade.View):
         image_source = "images/Alex Images/Owlet_Monster.png"
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
         self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 128
+        self.player_sprite.center_y = 400
         self.player_list.append(self.player_sprite)
 
-        # self.player_sprite = PlayerCharacter()
 
-        # self.player_sprite.center_x = 64
-        # self.player_sprite.center_y = 96
-        # self.player_sprite.scale = 0.8
+        # Name of map file to load
+        map_name = "images\Alex Images\map\map.tmx"
+        # Name of the layer in the file that has our platforms/walls
+        platforms_layer_name = 'Platforms'
+        # Name of the layer that has items for pick-up
+        coins_layer_name = 'Coins'
 
-        # self.player_list.append(self.player_sprite)
+        # Read in the tiled map
+        my_map = arcade.tilemap.read_tmx(map_name)
+
+        # -- Platforms
+        self.wall_list = arcade.tilemap.process_layer(my_map, platforms_layer_name, TILE_SCALING)
+
+        # -- Coins
+        self.coin_list = arcade.tilemap.process_layer(my_map, coins_layer_name, TILE_SCALING)
+
+        # --- Other stuff
+        # Set the background color
+        if my_map.background_color:
+            arcade.set_background_color(arcade.color.ICEBERG)
 
 
+ 
         # Used to keep track of our scrolling
         self.view_bottom = 0
         self.view_left = 0
 
         self.score = 0
 
-        self.background = arcade.load_texture("images/Alex Images/glacial_mountains_preview_lightened.png")
+        # self.background = arcade.load_texture("images/Alex Images/glacial_mountains_preview_lightened.png")
         
         # Create the ground
 
-        for x in range(0, settings.WIDTH, 64):
-            wall = arcade.Sprite("images/Alex Images/128x128/GrassJoinHillLeft&Right.png", TILE_SCALING)
-            wall.center_x = x
-            wall.center_y = 32
-            self.wall_list.append(wall)
+        # for x in range(-settings.WIDTH, settings.WIDTH, 64):
+        #     wall = arcade.Sprite("images/Alex Images/128x128/GrassJoinHillLeft&Right.png", TILE_SCALING)
+        #     wall.center_x = x
+        #     wall.center_y = 32
+        #     self.wall_list.append(wall)
 
-        coordinate_list = [[500, 200],
-                           [564, 200],
-                           [768, 96]]
+        # coordinate_list = [[500, 200],
+        #                    [564, 200],
+        #                    [768, 96]]
 
-        for coordinate in coordinate_list:
-            # Add a crate on the ground
-            wall = arcade.Sprite("images/Alex Images/128x128/GrassCliffMid.png", TILE_SCALING)
-            wall.position = coordinate
-            self.wall_list.append(wall)
-        # Adding Coins
+        # for coordinate in coordinate_list:
+        #     # Add a crate on the ground
+        #     wall = arcade.Sprite("images/Alex Images/128x128/GrassCliffMid.png", TILE_SCALING)
+        #     wall.position = coordinate
+        #     self.wall_list.append(wall)
+        # # Adding Coins
+
+
 
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
-        
+
+
+     
     def on_draw(self):
         arcade.start_render()
 
-        arcade.draw_texture_rectangle(settings.WIDTH // 2, settings.HEIGHT // 2,
-                                      settings.WIDTH, settings.HEIGHT, self.background)
 
-        arcade.draw_texture_rectangle(settings.WIDTH * 2, settings.HEIGHT * 2,
-                                      settings.WIDTH, settings.HEIGHT, self.background)
+                                      
 
         self.wall_list.draw()
         self.coin_list.draw()
 
         self.player_list.draw()
 
-        # Physics Engine
+
+
 
 
     def on_key_press(self, key, modifiers):
@@ -196,10 +153,7 @@ class Alexlevel(arcade.View):
         # Move the player with the physics engine
 
         self.physics_engine.update()
-        self.coin_list.update()
-        self.coin_list.update_animation()
-        self.player_list.update()
-        self.player_list.update_animation()
+
 
         changed = False
 
---
+++ images/Alex Images/map/winter_tiles.tsx
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="winter_tiles" tilewidth="128" tileheight="128" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="128" height="128" source="../../../../../vscode/cpt-2019-alex-s-shadow-realm/images/Alex Images/128x128/GrassCliffLeft.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../../../../../vscode/cpt-2019-alex-s-shadow-realm/images/Alex Images/128x128/GrassCliffMid.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="128" source="../../../../../vscode/cpt-2019-alex-s-shadow-realm/images/Alex Images/128x128/GrassCliffRight.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="128" source="../../../../../vscode/cpt-2019-alex-s-shadow-realm/images/Alex Images/128x128/GrassColumn.png"/>
+ </tile>
+ <tile id="4">
+  <image width="128" height="128" source="../../../../../vscode/cpt-2019-alex-s-shadow-realm/images/Alex Images/128x128/GrassHillLeft.png"/>
+ </tile>
+ <tile id="5">
+  <image width="128" height="128" source="../../../../../vscode/cpt-2019-alex-s-shadow-realm/images/Alex Images/128x128/GrassHillLeft2 DownShadow.png"/>
+ </tile>
+ <tile id="6">
+  <image width="128" height="128" source="../../../../../vscode/cpt-2019-alex-s-shadow-realm/images/Alex Images/128x128/GrassHillLeft2.png"/>
+ </tile>
+ <tile id="7">
+  <image width="128" height="128" source="../../../../../vscode/cpt-2019-alex-s-shadow-realm/images/Alex Images/128x128/GrassHillRight.png"/>
+ </tile>
+ <tile id="8">
+  <image width="128" height="128" source="../../../../../vscode/cpt-2019-alex-s-shadow-realm/images/Alex Images/128x128/GrassHillRight2 DownShadow.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="128" source="../../../../../vscode/cpt-2019-alex-s-shadow-realm/images/Alex Images/128x128/GrassHillRight2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../../../../../vscode/cpt-2019-alex-s-shadow-realm/images/Alex Images/128x128/GrassLeft.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../../../../../vscode/cpt-2019-alex-s-shadow-realm/images/Alex Images/128x128/GrassMid.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../../../../../vscode/cpt-2019-alex-s-shadow-realm/images/Alex Images/128x128/GrassRight.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="assets/GrassJoinHillRight2&amp;Left2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="assets/GrassJoinHillRight2&amp;Left2 DownShadow.png"/>
+ </tile>
+ <wangsets>
+  <wangset name="New Wang Set" tile="-1"/>
+  <wangset name="New Wang Set" tile="-1"/>
+ </wangsets>
+</tileset>
